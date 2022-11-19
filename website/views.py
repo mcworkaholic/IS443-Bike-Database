e5a955b8ca4d6e298a9805b46e3f7c0525a51298
mcworkaholic <wevans302@gmail.com>
2022-11-18 18:53:46 -0600

    got updates working + better exception handling

--- website/views.py
+++ website/views.py
@@ -28,7 +28,6 @@ sid = os.getenv('SID')
 sid = cx_Oracle.makedsn(host, port, sid=sid)
 pool = oracledb.create_pool(user=un, password = pw, dsn=sid, min=2, max=5, increment=1)
 
-
 @login_manager.user_loader
 def load_user(member_id):
     return User.query.get(int(member_id))
@@ -47,7 +46,6 @@ def shop():
                 cursor.execute(q1)
                 product_data = cursor.fetchall()
     return render_template("shop.html", data = product_data, user=current_user)
-
 
 @views.route("/checkout/<int:id>", methods=("GET", "POST"), strict_slashes=False)
 @login_required
@@ -72,7 +70,7 @@ def checkout(id:int):
                         update_cust_rentals = """UPDATE customer SET customer.num_rentals = ((SELECT num_rentals FROM customer WHERE customer.member_id =:1) -1)"""
                         update_bike_status = """UPDATE bike SET bike.status = 'in' WHERE bike.bike_id = :1"""
                         update_bike_location = """UPDATE bike SET bike.location_id = :1 WHERE bike.bike_id = :2"""
-                        update_balance = """UPDATE customer SET unpaid_balance = (SELECT unpaid_balance FROM customer WHERE customer.member_id = :1) +""" #work on
+                        update_balance = """UPDATE customer SET unpaid_balance = (SELECT unpaid_balance FROM customer WHERE customer.member_id = :1) + (SELECT (TRUNC(act_return) - TRUNC(exp_return))*20 FROM rental_detail INNER JOIN rental_bike on rental_bike.rental_id = rental_detail.rental_id WHERE member_id = :2 AND bike_id = :3)""" 
                         update_act_return = """UPDATE rental_detail SET act_return = :1 WHERE exp_return = (SELECT TRUNC(rented_out) FROM rental_bike WHERE bike_id = :2 AND member_id = :3) + (SELECT days_out FROM rental_bike WHERE bike_id = :4 AND member_id = :5)"""
                         update_location_return = """UPDATE rental_detail SET location_return = :1 WHERE exp_return = (SELECT TRUNC(rented_out) FROM rental_bike WHERE bike_id = :2 AND member_id =:3) + (SELECT days_out FROM rental_bike WHERE bike_id = :4 AND member_id = :5)"""
                         cursor.execute("ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MON-YYYY HH24:MI:SS'")
@@ -80,6 +78,7 @@ def checkout(id:int):
                         cursor.execute(update_act_return,[oracle_date, int(bike_id[-1])+1, current_user.member_id, int(bike_id[-1])+1, current_user.member_id])
                         cursor.execute(update_bike_location, [return_location, int(bike_id[-1])+1])
                         cursor.execute(update_cust_rentals, [current_user.member_id])
+                        cursor.execute(update_balance, [current_user.member_id, current_user.member_id, int(bike_id[-1])+1])
                         cursor.execute(update_bike_status, [int(bike_id[-1])+1])
                         connection.commit()
                 elif request.form['submit_button'] == 'Checkout':
@@ -87,34 +86,23 @@ def checkout(id:int):
                     with pool.acquire() as connection:
                         cursor = connection.cursor() 
                         cursor.execute("ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MON-YYYY HH24:MI:SS'")
-                        location_from = cursor.execute("""SELECT location_id FROM bike WHERE bike.bike_id =:bike""",bike=int(bike_id[-1])+1).fetchall()
-                        daily_fee = cursor.execute("""SELECT daily_fee FROM bike WHERE bike_id = :bike""",bike=int(bike_id[-1])+1).fetchall()
-                        exp_return = (datetime.datetime.date(datetime.datetime.now()) + timedelta(days=int(days))).strftime('%d-%b-%Y').upper() ## attempt to explain
                         insert_rental = """INSERT INTO rental_bike (member_id, bike_id, rented_out, days_out) VALUES(:1, :2, :3, :4)"""
-                        insert_rental_detail = """INSERT INTO rental_detail (rental_id, exp_return, location_from) VALUES(:1, :2, :3)"""
-                        update_balance = """UPDATE customer SET customer.unpaid_balance = (SELECT unpaid_balance FROM customer WHERE customer.member_id = :1) + (:2 * :3) WHERE customer.member_id = :4"""
-
                         try:
-                            cursor.execute(update_balance, [current_user.member_id, (daily_fee[0][0]),  days, current_user.member_id])
                             cursor.execute(insert_rental, [current_user.member_id, int(bike_id[-1])+1, datetime.datetime.now(), days])
-                            rental_id = cursor.execute("""SELECT rental_id FROM rental_bike WHERE member_id = :member_id AND bike_id=:bike_id""", member_id = current_user.member_id, bike_id=int(bike_id[-1])+1).fetchall()
-                            cursor.execute(insert_rental_detail, [rental_id[0][0], exp_return, location_from[0][0]])
                             connection.commit()
-                        except oracledb.IntegrityError:
+                        except oracledb.IntegrityError as e:
+                            error_obj, = e.args
                             db.session.rollback()
-                            if customer_data[0][0] == 2:
-                                flash(f"Maximum number of rentals reached.", "warning")
-                                return redirect(url_for('views.checkout', id=int(bike_id[-1])))
-                            elif  customer_data[0][0] !=2:
-                                flash(f"Maximum account balance reached.", "warning")
-                                return redirect(url_for('views.checkout', id=int(bike_id[-1])))
-                       
+                            flash(f"Maximum number of rentals reached.", "warning")
+                            return redirect(url_for('views.checkout', id=int(bike_id[-1])))
+                        except oracledb.DatabaseError as e:    
+                            flash(f"Maximum account balance reached.", "warning")
+                            db.session.rollback()
+                            return redirect(url_for('views.checkout', id=int(bike_id[-1])))
                 if days != None:
-                    return render_template("blank.html", p = " location_from: " + str(location_from[0][0]) +" rental_id: " + str(rental_id[0][0]) +" exp_return: " + str(exp_return) + " num_rentals: " + str(customer_data[0][0]) + " daily_fee: " + str(daily_fee[0][0]) + " days: " + str(days) + " member_id: " + str(member_id) + " bike_id: " + str(int(bike_id[-1])+1))
+                    return render_template("blank.html", p =  " num_rentals: " + str(customer_data[0][0])  + " days: " + str(days) + " member_id: " + str(member_id) + " bike_id: " + str(int(bike_id[-1])+1))
                 else:
                     return render_template("blank.html", p = str(customer_data[0])+ "  " +  "return_date: " + str(oracle_date) + " return_location: " + str(return_location)+ " member_id: " + str(member_id)+ " bike_id: "  + str(int(bike_id[-1])+1))
-
-     # No Work needed here 
     elif request.method == "GET":
         q1 = """SELECT * FROM bike INNER JOIN category ON bike.category_id = category.category_id INNER JOIN manufacturer ON bike.manufacturer_id = manufacturer.manufacturer_id ORDER BY bike_id"""
         q2 = """SELECT location_id, city, state FROM location"""
@@ -126,13 +114,9 @@ def checkout(id:int):
                     location_data = cursor.fetchall()
         return render_template("checkout.html", pdata = product_data[id], user=current_user, ldata=location_data)
 
-    
-
-
 @views.route("/login/", methods=("GET", "POST"), strict_slashes=False)
 def login():
     form = login_form()
-
     if form.validate_on_submit():
         try:
             user = User.query.filter_by(email=form.email.data).first()
@@ -143,14 +127,12 @@ def login():
                 flash("Invalid Username or password!", "danger")
         except Exception as e:
             flash(e, "danger")
-
     return render_template("auth.html",
         form=form,
         text="Login",
         title="Login",
         btn_action="Login"
         )
-
 
 # Register route
 @views.route("/register/", methods=("GET", "POST"), strict_slashes=False)
@@ -164,8 +146,7 @@ def register():
             phone = form.phone.data
             address = form.address.data
             password = form.password.data
-            
-            
+
             newuser = User(
                 email=email,
                 fname=fname,
@@ -176,7 +157,7 @@ def register():
                 num_rentals = 0,
                 password=bcrypt.generate_password_hash(password).decode('utf8'),
             )
-    
+
             db.session.add(newuser)
             db.session.commit()
             flash("Account Creation Successful.", "success")
@@ -206,7 +187,6 @@ def register():
         title="Register",
         btn_action="Register account"
         )
-
 @views.route("/logout")
 @login_required
 def logout():
